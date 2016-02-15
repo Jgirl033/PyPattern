2deb79f0ee675b9b17ef2db73ab71daf96b9aa3e
Jgirl <18819423747@163.com>
2016-02-15 21:40:48 +0800

    Add class Pet and PetFactory

--- abstract_factory.py
+++ abstract_factory.py
@@ -9,8 +9,65 @@
 import random
 
 
+# 抽象产品类
+class Pet:
+    def __init__(self):
+        pass
+
+    def speak(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+# 具体产品类
+class Dog(Pet):
+    def speak(self):
+        return "woof"
+
+    def __str__(self):
+        return "Dog"
+
+
+class Cat(Pet):
+    def speak(self):
+        return "meow"
+
+    def __str__(self):
+        return "Cat"
+
+
 # 抽象工厂类
-class PetShop:  # 宠物类
+class PetFactory:
+    def __init__(self):
+        pass
+
+    def get_pet(self):
+        pass
+
+    def get_food(self):
+        pass
+
+
+# 具体工厂类
+class DogFactory(PetFactory):  # 狗厂类
+    def get_pet(self):
+        return Dog()
+
+    def get_food(self):
+        return "dog food"
+
+
+class CatFactory(PetFactory):  # 猫厂类
+    def get_pet(self):
+        return Cat()
+
+    def get_food(self):
+        return "cat food"
+
+
+class PetShop:  # 根据确定的工厂生产产品
     def __init__(self, animal_factory=None):
         self.pet_factory = animal_factory
 
@@ -21,46 +78,7 @@ class PetShop:  # 宠物类
         print("It eats", self.pet_factory.get_food())
 
 
-class Dog:
-    def __init__(self):
-        pass
-
-    def speak(self):
-        return "woof"
-
-    def __str__(self):
-        return "Dog"
-
-
-class Cat:
-    def __init__(self):
-        pass
-
-    def speak(self):
-        return "meow"
-
-    def __str__(self):
-        return "Cat"
-
-
-# 具体工厂类
-class DogFactory:  # 狗厂类
-    def get_pet(self):
-        return Dog()
-
-    def get_food(self):
-        return "dog food"
-
-
-class CatFactory:  # 猫厂类
-    def get_pet(self):
-        return Cat()
-
-    def get_food(self):
-        return "cat food"
-
-
-def get_factory():
+def get_factory():  # 确定到底是需要什么工厂
     return random.choice([DogFactory, CatFactory])()
 
 
